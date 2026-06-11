252c371739cb15dbc6642a145ca1bcd448c3e1b8
Ahmadreza Ansari <ahmadreza.an.dev@gmail.com>
2026-06-11 22:08:06 +0330

    update fixed

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,10 +7,8 @@ const Contact = () => {
   return (
     <section className='section contact center' id='contact'>
       <h2 className='section__title'>Contact</h2>
-      <a href={`mailto:${contact.email}`}>
-        <span type='button' className='btn btn--outline'>
-          Email me
-        </span>
+      <a href={`mailto:${contact.email}`} className='btn btn--outline'>
+        Email me
       </a>
     </section>
   )
